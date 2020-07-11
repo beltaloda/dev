7765be8933d88b112b3a3878aa623d48f7c38773
beltaloda <61656409+beltaloda@users.noreply.github.com>
2020-07-11 15:59:49 +0300

    Add files via upload

---
+++ catalog.list/lang/ru/.parameters.php
@@ -0,0 +1,23 @@
+<?
+$MESS["IBLOCK_ACTIONS"] = "Настройки действий";
+$MESS["SORT_SETTINGS"] = "Настройки сортировки";
+$MESS["IBLOCK_TYPE"] = "Тип инфоблока";
+$MESS["IBLOCK_IBLOCK"] = "инфоблок";
+$MESS["IBLOCK_PROPERTY"] = "Свойства";
+$MESS["IBLOCK_SORT_ASC"] = "по возрастанию";
+$MESS["IBLOCK_SORT_DESC"] = "по убыванию";
+$MESS["IBLOCK_LINE_ELEMENT_COUNT"] = "Количество элементов выводимых в одной строке таблицы";
+$MESS["IBLOCK_ELEMENT_SORT_FIELD"] = "По какому полю сортируем элементы";
+$MESS["IBLOCK_ELEMENT_SORT_ORDER"] = "Порядок сортировки элементов";
+$MESS["IBLOCK_ELEMENT_SORT_FIELD2"] = "Поле для второй сортировки элементов";
+$MESS["IBLOCK_ELEMENT_SORT_ORDER2"] = "Порядок второй сортировки элементов";
+$MESS["IBLOCK_PAGE_ELEMENT_COUNT"] = "Количество элементов на странице";
+$MESS["CP_BCS_INCLUDE_SUBSECTIONS"] = "Показывать элементы подразделов раздела";
+$MESS["CP_BCS_SHOW_ALL_WO_SECTION"] = "Показывать все элементы, если не указан раздел";
+$MESS["CP_BCS_CACHE_GROUPS"] = "Учитывать права доступа";
+$MESS["CP_BCS_INCLUDE_SUBSECTIONS_ALL"] = "всех подразделов";
+$MESS["CP_BCS_INCLUDE_SUBSECTIONS_ACTIVE"] = "активных подразделов";
+$MESS["CP_BCS_INCLUDE_SUBSECTIONS_NO"] = "не показывать";
+$MESS["CP_BCS_USE_MAIN_ELEMENT_SECTION"] = "использовать основной раздел для показа элемента";
+$MESS["CP_BCS_EMPTY"] = "не выбрано";
+?>
---
+++ catalog.list/lang/ru/component.php
@@ -0,0 +1,8 @@
+<?
+$MESS["IBLOCK_NOT_SELECTED"]= "Не выбран инфоблок";
+$MESS["SUCCESS_ADD"]= "Город добавлен";
+$MESS["ERR_ADD"]= "Не введено назване";
+$MESS["SUCCESS_DEL"]= "Город удален";
+$MESS["DEL_ERR"]= "Ошибка удаления";
+$MESS["DELETE"]= "Удалить";
+?>
